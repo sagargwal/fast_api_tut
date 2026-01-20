8ae2f792246a423d5cb2a28c7641ff7d32dac16d
sagargwal <sagargwal34@gmail.com>
2026-01-20 17:55:40 +0530

    with post

--- medical_profiler/main.py
+++ medical_profiler/main.py
@@ -1,13 +1,81 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 import json
+from fastapi.responses import JSONResponse
+from pydantic  import BaseModel, Field, computed_field
+from typing import Annotated, Literal, Optional
+
+
 
 app = FastAPI()
+
+
+class patient(BaseModel):
+    # Annotated is a typing wrapper class it is not a function nor a class and  stores a base type and metadata so tools like Pydantic can enforce rules later.
+    '''
+    this is how annotated will look like 
+    class FakeAnnotated:
+    def __init__(self, base_type, *metadata):
+        self.base_type = base_type
+        self.metadata = metadata'''
+    # the reason why wee are not using comma after each variable is because they are independent and does not work as arguments of a function
+    #  the reason why str and fields are stored in [] and not in () is because it is not  callable class that does something and str and field does not go it .__init__ but it goes in .__getitems__()
+    id : Annotated[str, Field(..., description="id of the patient", examples=["P001"])]
+    name : Annotated[str, Field(..., description="name of the patient")]
+    city : Annotated[str, Field(..., description="city of the patient")]
+    age : Annotated[int, Field(...,gt=0 , lt= 120,description="age of the patient")]
+    gender : Annotated[Literal["male","female","others"],Field(...,description="gender of the patient")]
+    height : Annotated[float, Field(...,gt=0, description="height of the patient in mtrs")]
+    weight : Annotated[float, Field(...,gt=0, description="weight of the patient in kgs")]
+
+
+    '''@property & @computed_field — very short notes (final)
+
+    @property → access method like a variable (patient.bmi)
+
+    Without @property → patient.bmi is a method, not a value
+
+    @computed_field → include field in model_dump() / JSON
+
+    Without @property → model_dump() will NOT include bmi
+
+    Both together → value auto-calculates and appears in JSON
+
+    One-line rule:
+
+    @property makes it readable, @computed_field makes it exportable.'''
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2),2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Normal'
+        else:
+            return 'Obese'
+
+
+
+
 
 def load_data():
     with open("C:/Users/makerofdreams/Desktop/Fast_api/medical_profiler/patients.json","r") as f:# here with "with" the document opens and closes by itself 
     # the open function opens it and "r" stands for read only 
         data = json.load(f) #json.load helps understand the raw data works as a parser
     return data
+
+def save_data(data):
+    with open('patients.json', 'w') as f:
+        json.dump(data, f)
+    
 
 
 @app.get("/")
@@ -59,3 +127,25 @@ def sort_patients(sort_by : str = Query(...,description="this endpoint lets the 
     sorted_dict = sorted(data.values(),key = lambda x:x.get(sort_by,0),reverse= sorted_order)
 
     return sorted_dict
+
+
+@app.post('/create')
+def create_patient(patient: patient):
+
+    # load existing data
+    data = load_data()
+
+    # check if the patient already exists
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail='Patient already exists')
+
+    # new patient add to the database
+    data[patient.id] = patient.model_dump(exclude=['id'])
+
+    # save into the json file
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={'message':'patient created successfully'})
+
+
+    
